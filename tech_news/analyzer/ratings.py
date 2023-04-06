7396e49157e3c486f654032471b640ec96819099
JaySant <jaysant15@gmail.com>
2023-04-06 01:27:33 -0300

    feat: requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,19 @@
+from tech_news.database import get_collection
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    list = get_collection().aggregate(
+        [
+            {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1, "_id": 1}},
+        ]
+    )
+
+    if not list:
+        return []
+
+    results = []
+    for category in list:
+        results.append((category["_id"]))
+    return results
